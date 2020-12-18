9621500d440f993ebd9c22e9b1b6b80bc68295ae
nikhil-kumar-hash <75771313+nikhil-kumar-hash@users.noreply.github.com>
2020-12-19 00:47:46 +0530

    Update creds.go

--- creds.go
+++ creds.go
@@ -8,8 +8,8 @@ type Creds struct {
 
 func GetCredentials() Creds {
 	return Creds{
-		AccessKey: "AKIATYO76VW7LIH25VP3",                     //Load your access key here
-		SecretKey: "U8LvZDHDa9USFEDgsT4QqTY0rHzfgPQ51G5jcCs9", //Load your secret key here
+		AccessKey: "",                     //Load your access key here
+		SecretKey: "", //Load your secret key here
 		Region:    "us-east-2",                                //Load your region here
 	}
 }
